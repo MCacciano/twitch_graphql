0b4815008a0579efcb005744d5aa15a9c2552091
michael.cacciano@vmlyr.com <michael.cacciano@vmlyr.com>
2019-10-23 00:03:20 -0400

    updated routes

--- server/datasources/twitch/twitch.js
+++ server/datasources/twitch/twitch.js
@@ -5,52 +5,22 @@ class TwitchAPI extends RESTDataSource {
 	constructor() {
 		super();
 		this.baseURL = `https://api.twitch.tv/helix`;
-		// ! enter streamers' user_id to get streamer specific data
+		// enter streamers' user_id to get streamer specific data
 		this.user_id = '63801297';
-		// ! no auth without twitch Client ID - this will come from twitch after registering a new app
+		// no auth without twitch Client ID - this will come from twitch after registering a new app
 		this.headers = {
 			'Client-ID': process.env.TWITCH_CLIENT_ID
 		};
 	}
 
-	async asyncGet(url = '', params = {}, { noUserId = false }) {
-		const { user_id, headers } = this;
-
-		if (url.length === 0) {
-			console.error('Please enter a url');
-			return;
-		}
-
-		if (noUserId === false) {
-			params = {
-				...params,
-				user_id
-			};
-		}
-
-		console.log(params);
-
-		try {
-			const res = await this.get(url, params, { headers });
-			return res;
-		} catch (err) {
-			console.error(err);
-		}
+	$get(url) {
+		return this.get(url, { user_id: this.user_id }, { headers: this.headers });
 	}
 
 	async videos() {
-		const videos = await this.asyncGet(`/videos`);
+		const videos = await this.$get(`/videos`);
+		console.log(videos);
 		return Array.isArray(videos.data) ? videos.data.map(video => videoReducer(video)) : [];
-	}
-
-	// * technically you can request multiple videos by ID but this has been setup to only use
-	// * one video ID and return that first and only video from the response array
-	async videoById({ id = '' }) {
-		const video = await this.asyncGet(`/videos`, { id }, { noUserId: true });
-
-		console.log(video);
-
-		return Array.isArray(video.data) ? videoReducer(video.data[0]) : [];
 	}
 }
 
